941daa7670a9995296a8f7947824687f1b5be93c
Minkyu Park <rb6609@naver.com>
2025-03-06 13:26:34 +0900

    refactor: LoginService 회원가입 및 로그인 로직 리팩토링

--- src/main/java/com/arom_policy/demo/login/service/LoginService.java
+++ src/main/java/com/arom_policy/demo/login/service/LoginService.java
@@ -1,18 +1,14 @@
 package com.arom_policy.demo.login.service;
 
-import com.arom_policy.demo.login.common.BaseException;
-import com.arom_policy.demo.login.common.BaseResponseStatus;
-import com.arom_policy.demo.login.dto.LoginResponseDto;
 import com.arom_policy.demo.login.entity.User;
 import com.arom_policy.demo.login.repository.UserRepository;
 import com.arom_policy.demo.login.token.JwtProvider;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -23,69 +19,51 @@ public class LoginService {
 
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
+    private final TokenService tokenService;
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    public LoginResponseDto getUserInfo(Long kakaoId) {
-        User user = userRepository.findByKakaoId(kakaoId)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
-
-        // JWT 발급
-        String accessToken = jwtProvider.createAccessToken(kakaoId);
-
-        return new LoginResponseDto(user.getId(), accessToken, user.getUserName());
-    }
-
-    public LoginResponseDto register(HashMap<String, Object> userInfo) {
+    public User register(HashMap<String, Object> userInfo) {
         Long kakaoId = Long.valueOf(userInfo.get("id").toString());
         String username = userInfo.get("username").toString();
 
         User newUser = new User();
         newUser.setKakaoId(kakaoId);
         newUser.setUserName(username);
-
-        userRepository.save(newUser);
-
-        // JWT 발급
-        String accessToken = jwtProvider.createAccessToken(kakaoId);
-
-        // 회원가입 성공 응답 반환
-        return new LoginResponseDto(newUser.getId(), accessToken, newUser.getUserName());
+        return userRepository.save(newUser);
     }
 
+    public ResponseEntity<Void> loginOrRegister(HashMap<String, Object> userInfo) {
+        Long kakaoId = Long.valueOf(userInfo.get("id").toString());
 
-    public int checkKakaoId(Long kakaoId) {
-        return userRepository.existsByKakaoId(kakaoId) ? 1 : 0;
+        // 기존 유저 조회 or 회원가입
+        User user = userRepository.findByKakaoId(kakaoId)
+                .orElseGet(() -> register(userInfo));
+
+        // JWT 생성
+        String jwt = jwtProvider.createAccessToken(user.getKakaoId());
+
+        // HTTP-Only 쿠키 설정
+        ResponseCookie jwtCookie = ResponseCookie.from("jwt", jwt)
+                .httpOnly(true)
+                .secure(false)    // HTTPS에서만 사용(로컬 개발이므로 false)
+                .path("/")
+                .maxAge(60 * 60) // 1시간 유지
+                .build();
+        log.info("{} : {}", user, jwtCookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString()) // 쿠키 설정
+                .build();
     }
 
-    public HashMap<String, Object> getUserInfo(String accessToken) {
-        HashMap<String, Object> userInfo = new HashMap<>();
-        String url = "https://kapi.kakao.com/v2/user/me";
+    public ResponseEntity<Void> loginWithKakao(String code) {
 
-        // HTTP 요청 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        String accessToken = tokenService.getKakaoAccessToken(code);
+        log.info("카카오 Access Token = {} ", accessToken);
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        // 카카오 유저 정보 요청
+        HashMap<String, Object> userInfo = tokenService.getUserInfoFromToken(accessToken);
+        log.info("카카오 유저 정보 = {} ",userInfo);
 
-        try {
-            // API 요청 실행
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-
-            // JSON 응답 파싱
-            JsonNode root = objectMapper.readTree(response.getBody());
-            String username = root.path("properties").path("nickname").asText();
-            Long kakaoId = root.path("id").asLong();
-
-            // 사용자 정보 저장
-            userInfo.put("username", username);
-            userInfo.put("id", kakaoId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return userInfo;
+        return loginOrRegister(userInfo);
     }
-
 }

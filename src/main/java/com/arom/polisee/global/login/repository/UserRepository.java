4f782383fd5e848743633daccd9d8613356f1ab2
Minkyu Park <rb6609@naver.com>
2025-03-13 20:13:17 +0900

    [Feat] findByUserName 함수 추가

--- src/main/java/com/arom/polisee/global/login/repository/UserRepository.java
+++ src/main/java/com/arom/polisee/global/login/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.arom.polisee.global.login.repository;
 
-import com.arom.polisee.global.login.entity.User;
+import com.arom.polisee.global.login.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByKakaoId(Long kakaoId);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByKakaoId(Long kakaoId);
+
+    Optional<UserEntity> findByUserName(String username);
 }

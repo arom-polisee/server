05062adb64afadac185e1a87b273c9f6dea70298
Minkyu Park <rb6609@naver.com>
2025-03-13 20:06:02 +0900

    [Feat] 401 Unauthorized 상태 추가

--- src/main/java/com/arom/polisee/global/login/common/BaseResponseStatus.java
+++ src/main/java/com/arom/polisee/global/login/common/BaseResponseStatus.java
@@ -1,12 +1,11 @@
 package com.arom.polisee.global.login.common;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public enum BaseResponseStatus {
     SUCCESS(200, "요청 성공"),
+    SC_UNAUTHORIZED(401,"토큰 인증 실패"),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(500, "서버 오류 발생");
 
@@ -16,8 +15,5 @@ public enum BaseResponseStatus {
     BaseResponseStatus(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
-    }
-    public String getMessage() {
-        return message;
     }
 }

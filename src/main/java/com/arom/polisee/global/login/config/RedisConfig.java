9f0ff77ac8e22d140ead13dcc5c7670d6931e316
Minkyu Park <rb6609@naver.com>
2025-03-19 21:02:29 +0900

    [refactor]  ObjectMapper 설정 개선

--- src/main/java/com/arom/polisee/global/login/config/RedisConfig.java
+++ src/main/java/com/arom/polisee/global/login/config/RedisConfig.java
@@ -1,12 +1,15 @@
 package com.arom.polisee.global.login.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -15,9 +18,13 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
+        //  key를 String으로 저장
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
         // ObjectMapper 설정
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, false); // 한글 깨짐 방지
 
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         return template;

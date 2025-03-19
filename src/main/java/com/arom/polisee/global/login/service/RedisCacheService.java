d7fc01db70938d75d791f7bde5585514536e9e36
Minkyu Park <rb6609@naver.com>
2025-03-19 21:09:30 +0900

    [refactor]  Redis 캐싱을 UserDto 기반으로 변경 및 로깅 추가

--- src/main/java/com/arom/polisee/global/login/service/RedisCacheService.java
+++ src/main/java/com/arom/polisee/global/login/service/RedisCacheService.java
@@ -1,9 +1,12 @@
 package com.arom.polisee.global.login.service;
 
 import com.arom.polisee.global.login.dto.CustomUserDetails;
+import com.arom.polisee.global.login.dto.UserDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,31 +14,38 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RedisCacheService {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
     private static final long CACHE_EXPIRE_TIME = 30; // 30분
     private static final String USER_KEY_PREFIX = "USER:";
 
-    public void saveUserDetails(CustomUserDetails customUserDetails) {
-        String key = getUserKey(customUserDetails.getId());
-        redisTemplate.opsForValue().set(key + customUserDetails.getId(), customUserDetails, CACHE_EXPIRE_TIME, TimeUnit.MINUTES);
+    public void saveUserDetails(UserDto userDto) {
+        String key = getUserKey(userDto.getId());
+
+        try {
+            log.info("Redis 저장 Key: {}, Value: {}", key, objectMapper.writeValueAsString(userDto));
+        } catch (JsonProcessingException e) {
+            log.error("JSON 변환 오류: {}", e.getMessage());
+        }
+
+        redisTemplate.opsForValue().set(key, userDto, CACHE_EXPIRE_TIME, TimeUnit.MINUTES);
     }
 
-    public CustomUserDetails getUserDetails(Long userId) {
+    public UserDto getUserDetails(Long userId) {
         String key = getUserKey(userId);
         Object data = redisTemplate.opsForValue().get(key);
         if (data == null) {
             return null;
         }
-        //  LinkedHashMap을 CustomUserDetails로 변환
-        return objectMapper.convertValue(data, CustomUserDetails.class);
+        return objectMapper.convertValue(data, UserDto.class);
     }
 
     private String getUserKey(Long userId) {
         return String.format("%s%d", USER_KEY_PREFIX, userId);
     }
     public void deleteUserDetails(Long userId) {
-        redisTemplate.delete("USER_" + userId);
+        redisTemplate.delete(USER_KEY_PREFIX + userId);
     }
 }

a1a63471e790f6e948f0586504e9f87219f6ed0d
Minkyu Park <rb6609@naver.com>
2025-03-27 18:00:45 +0900

    [refactor] loginWithKakao 메서드 로직 변경

--- src/main/java/com/arom/polisee/global/login/service/LoginService.java
+++ src/main/java/com/arom/polisee/global/login/service/LoginService.java
@@ -1,10 +1,10 @@
 package com.arom.polisee.global.login.service;
 
+import com.arom.polisee.domain.user.Role;
+import com.arom.polisee.domain.user.User;
 import com.arom.polisee.global.login.dto.LoginResultDto;
 import com.arom.polisee.global.login.dto.UserDto;
 import com.arom.polisee.global.login.dto.KakaoResponseDto;
-import com.arom.polisee.global.login.entity.Role;
-import com.arom.polisee.global.login.entity.UserEntity;
 import com.arom.polisee.global.login.repository.UserRepository;
 import com.arom.polisee.global.login.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -33,31 +33,33 @@ public class LoginService {
         log.info("카카오 유저 정보 : {} ", userInfo);
 
         // 로그인 or 회원가입
-        UserEntity user = loginOrRegister(userInfo);
+        User user = loginOrRegister(userInfo);
+        log.info("로그인 or 회원가입 완료 : {}",user);
 
         // JWT 생성
         String jwt = jwtProvider.createAccessToken(UserDto.fromEntity(user));
+        log.info("jwt 생성 완료 : {}", jwt);
 
         // 응답 바디에 JWT 포함 (클라이언트에서 저장할 수 있도록)
-        LoginResultDto responseDto = new LoginResultDto(jwt, user.getUsername(), user.getRole().name());
+        LoginResultDto responseDto = new LoginResultDto(jwt, user.getUserName(), user.getRole().name());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt) // 헤더에 JWT 추가
                 .body(responseDto);
     }
 
-    private UserEntity loginOrRegister(KakaoResponseDto userInfo) {
-        UserEntity user = userRepository.findByKakaoId(userInfo.getKakaoId());
+    private User loginOrRegister(KakaoResponseDto userInfo) {
+        User user = userRepository.findByKakaoId(userInfo.getKakaoId());
         if (user == null) {
             return register(userInfo);
         }
         return user;
     }
 
-    private UserEntity register(KakaoResponseDto userInfo) {
-        UserEntity newUserEntity = new UserEntity();
+    private User register(KakaoResponseDto userInfo) {
+        User newUserEntity = new User();
         newUserEntity.setKakaoId(userInfo.getKakaoId());
-        newUserEntity.setUsername(userInfo.getUsername());
+        newUserEntity.setUserName(userInfo.getUsername());
         newUserEntity.setRole(Role.ROLE_USER);
         return userRepository.save(newUserEntity);
     }

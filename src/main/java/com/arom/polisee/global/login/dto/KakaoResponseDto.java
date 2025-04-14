94080254e6c45c64587adff5769f89099e96d93e
Minkyu Park <rb6609@naver.com>
2025-04-14 12:49:53 +0900

    [refactor] 로그 출력을 위한 ToString 어노테이션 추가

--- src/main/java/com/arom/polisee/global/login/dto/KakaoResponseDto.java
+++ src/main/java/com/arom/polisee/global/login/dto/KakaoResponseDto.java
@@ -3,10 +3,12 @@ package com.arom.polisee.global.login.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class KakaoResponseDto {
     private Long kakaoId;
     private String username;
